511e645cdec124cfdb37c8861bf63bf5cc516e03
Ryan Resella <ryan@codeforamerica.org>
2011-09-25 11:45:53 -0700

    Added spec for an incoming call

--- spec/yakbus_spec.rb
+++ spec/yakbus_spec.rb
@@ -3,30 +3,31 @@ require File.dirname(__FILE__) + '/spec_helper'
 describe 'Yakbus Application' do
 
   describe '/index.json' do
-    it "should respond to an incoming text message" do
-      json = '{
-                "session":{
-                  "id":"1aa06515183223ec0894039c2af433f2",
-                  "accountId":"33932",
-                  "timestamp":"2010-02-18T19:07:36.375Z",
+    it "should respond to an incoming call" do
+      json = '{"session":
+                {"id":"d7a7f84ee0b497e73d152a62c99b1fc9",
+                  "accountId":"12345",
+                  "timestamp":"2011-09-25T18:36:30.737Z",
                   "userType":"HUMAN",
-                  "initialText":"10246",
+                  "initialText":null,
+                  "callId":"abc123",
                   "to":{
-                    "id":"9991427645",
-                    "name":"unknown",
-                    "channel":"TEXT",
-                    "network":"SMS"
-                      },
+                    "id":"6615551234",
+                    "name":"+16615551234",
+                    "channel":"VOICE",
+                    "network":"SIP"
+                    },
                   "from":{
-                    "id":"16615551234",
-                    "name":null,
-                    "channel":"TEXT",
-                    "network":"SMS"
-                      }
+                    "id":"661-4444-5555",
+                    "name":"+16614445555",
+                    "channel":"VOICE",
+                    "network":"SIP"
+                    }
                   }
-            }'
+              }'
+
       post '/index.json',json
-      last_response.body.should == "{\"tropo\":[{\"ask\":{\"name\":\"initial_text\",\"choices\":{\"value\":\"[ANY]\"}}},{\"on\":{\"event\":\"continue\",\"next\":\"/continue.json\"}}]}"
+      last_response.body.should == "{\"tropo\":[{\"ask\":{\"name\":\"digit\",\"timeout\":60,\"say\":{\"value\":\"Enter the five digit bus stop number\"},\"choices\":{\"value\":\"[5 DIGITS]\"}}},{\"on\":{\"event\":\"continue\",\"next\":\"/continue.json\"}}]}"
     end
   end
 
