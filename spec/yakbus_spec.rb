f738c39d6e845b639d2881a4f1d4c5643da932b2
Ryan Resella <ryan@codeforamerica.org>
2011-09-25 19:16:07 -0700

    Wrote spec for sms over another jurisdiction

--- spec/yakbus_spec.rb
+++ spec/yakbus_spec.rb
@@ -4,7 +4,7 @@ describe 'Yakbus Application' do
 
   before(:all) do
     set :sender_phone, '555-555-1212'
-    set :va_phone, '15555551234'
+    set :va_phone, '+15711234567'
     set :char_phone, '15551234567'
   end
 
@@ -88,7 +88,7 @@ describe 'Yakbus Application' do
   end
 
   describe '/sms_incoming.json' do
-    it "should respond to an incoming text message" do
+    it "should respond to an incoming text message for Santa Clarita" do
       json = '{
                 "session":{
                   "id":"1aa06515183223ec0894039c2af433f2",
@@ -97,7 +97,7 @@ describe 'Yakbus Application' do
                   "userType":"HUMAN",
                   "initialText":"10246",
                   "to":{
-                    "id":"9991427645",
+                    "id":"155551234",
                     "name":"unknown",
                     "channel":"TEXT",
                     "network":"SMS"
@@ -116,6 +116,37 @@ describe 'Yakbus Application' do
       last_response.body.should == "{\"tropo\":[{\"say\":[{\"value\":\"Route 1-Destination Castaic-ETA 24 minutes Route 4-Destination LARC-ETA 19 minutes Route 6-Destination Shadow Pines-ETA 17 minutes Route 14-Destination Plum Cyn-ETA 11 minutes \"}]},{\"hangup\":null},{\"on\":{\"event\":\"hangup\",\"next\":\"/hangup.json\"}}]}"
     end
   end
+
+  describe '/sms_incoming.json' do
+    it "should respond to an incoming text message for Arlington, VA" do
+      json = '{
+                "session":{
+                  "id":"1aa06515183223ec0894039c2af433f2",
+                  "accountId":"33932",
+                  "timestamp":"2010-02-18T19:07:36.375Z",
+                  "userType":"HUMAN",
+                  "initialText":"87017",
+                  "to":{
+                    "id":"15711234567",
+                    "name":"unknown",
+                    "channel":"TEXT",
+                    "network":"SMS"
+                      },
+                  "from":{
+                    "id":"16615551234",
+                    "name":null,
+                    "channel":"TEXT",
+                    "network":"SMS"
+                      }
+                  }
+            }'
+      stub_request(:get, "http://realtime.commuterpage.com/rtt/public/utility/file.aspx?Name=RoutePositionET.xml&contenttype=SQLXML&platformno=87017").
+        to_return(:status => 200, :body => fixture("va_single.xml"))
+      post '/sms_incoming.json',json
+      last_response.body.should == "{\"tropo\":[{\"say\":[{\"value\":\"Route 87 -Destination Shirlington Station -ETA 17 min\"}]},{\"hangup\":null},{\"on\":{\"event\":\"hangup\",\"next\":\"/hangup.json\"}}]}"
+    end
+  end
+
 
   describe "hangup.json" do
     it "should return the hangup info" do
