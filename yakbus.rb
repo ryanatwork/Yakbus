e7a9720eea98f1b66e115e57bda7d20dca93a72e
Ryan Resella <ryan@codeforamerica.org>
2011-12-17 15:21:10 -0800

    Shorten text messages

--- yakbus.rb
+++ yakbus.rb
@@ -236,45 +236,66 @@ end
 
 def get_et_info(location,platform)
 
-  if location == "va"
-    @client = Connexionz::Client.new({:endpoint => "http://realtime.commuterpage.com"})
-  elsif location == "char"
-   @client = Connexionz::Client.new({:endpoint => "http://avlweb.charlottesville.org"})
-  else
-    @client = Connexionz::Client.new({:endpoint => "http://12.233.207.166"})
-  end
+  begin
+    if location == "va"
+      @client = Connexionz::Client.new({:endpoint => "http://realtime.commuterpage.com"})
+    elsif location == "char"
+      @client = Connexionz::Client.new({:endpoint => "http://avlweb.charlottesville.org"})
+    else
+      @client = Connexionz::Client.new({:endpoint => "http://12.233.207.166"})
+    end
 
-   @platform_info = @client.route_position_et({:platformno => platform})
+    @platform_info = @client.route_position_et({:platformno => platform})
 
-   if @platform_info.route_position_et.platform.nil?
-     sms_message = "No bus stop found"
-   else
+    if @platform_info.route_position_et.platform.nil?
+      sms_message = "No bus stop found"
+    else
       name = @platform_info.route_position_et.platform.name
       arrival_scope = @platform_info.route_position_et.content.max_arrival_scope
       sms_message = ""
       eta = ""
       if @platform_info.route_position_et.platform.route.nil?
         sms_message = "No arrivals for next #{arrival_scope} minutes"
-      elsif @platform_info.route_position_et.platform.route.class == Array
+      elsif @platform_info.route_position_et.platform.route.is_a?(Array)
         @platforms = @platform_info.route_position_et.platform.route
         @platforms.each do |platform|
-          sms_message += "Route #{platform.route_no}-Destination #{platform.destination.name}-ETA #{platform.destination.trip.eta } minutes "
+          if platform.destination.is_a?(Array)
+            platform.destination.each do |dest|
+              sms_message += "#{platform.route_no}-#{dest.name}"
+              sms_message += "-ETA:#{multi_eta(dest.trip)} "
+            end
+        else
+          sms_message += "#{platform.route_no}-#{platform.destination.name}"
+          sms_message += "-ETA:#{multi_eta(platform.destination.trip)} "
         end
-      else
-        route_no = @platform_info.route_position_et.platform.route.route_no
-        destination = @platform_info.route_position_et.platform.route.destination.name
-        if @platform_info.route_position_et.platform.route.destination.trip.is_a?(Array)
-         @platform_info.route_position_et.platform.route.destination.trip.each do |mult_eta|
-           eta += "#{mult_eta.eta} min "
-         end
-       else
-         eta = "#{@platform_info.route_position_et.platform.route.destination.trip.eta} min"
-       end
-       sms_message = "Route #{route_no} " + "-Destination #{destination} " + "-ETA #{eta}"
       end
-   end
-  sms_message
- end
+    else
+      route_no = @platform_info.route_position_et.platform.route.route_no
+      destination = @platform_info.route_position_et.platform.route.destination.name
+      eta = multi_eta(@platform_info.route_position_et.platform.route.destination.trip)
+      sms_message = "#{route_no}-#{destination}-ETA:#{eta}"
+    end
+  end
+
+  rescue
+    sms_message = "An error has occured please try again"
+  end
+    sms_message.rstrip
+end
+
+def multi_eta(eta)
+  multi_eta = ""
+  arr_eta = []
+  if eta.is_a?(Array)
+    eta.each do |mult_eta|
+      arr_eta.push(mult_eta.eta)
+      multi_eta = arr_eta.join(',')
+    end
+  else
+    multi_eta = eta.eta
+  end
+  multi_eta
+end
 
 
 ##################
